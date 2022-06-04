77a96769e4125d4327a3726e9961ca48f42cbb4b
Volodymyr Dorozhovets <dorozovecvoloda@gmail.com>
2022-06-04 15:55:03 +0300

    added default data to the main_data.py script

---
+++ scripts/main_data.py
@@ -0,0 +1,28 @@
+# --------------------------------------------------------------- #
+# default values for the script
+DEFAULT_DIRECTORY_ERROR_MESSAGE = "Directory does not exist: "
+
+DEFAULT_TEXT_ALREADY_PRESENT = "Text is already present in file: "
+DEFAULT_TEXT_NOT_PRESENT     = "Text is not present in file: "
+
+DEFAULT_TEXT_REMOVED = "Text removed from file: "
+DEFAULT_TEXT_ADDED   = "Text added to file: "
+
+DEFAULT_NO_FILES_WITH_EXTENSIONS_ERROR_MESSAGE = "No files with specified extensions found in directory: "
+
+DEFAULT_PARAMETER_ERROR_MESSAGE = "Please provide either -add or -remove parameter."
+
+DEFAULT_ADD_AND_REMOVE_MESSAGE = "Add and remove text in specified directories."
+
+DEFAULT_DIRECTORIES_TO_ADD_MESSAGE    = "Directories where text should be added."
+DEFAULT_DIRECTORIES_TO_REMOVE_MESSAGE = "Directories where text should be removed."
+
+DEFAULT_EXTENSIONS_MESSAGE = "Extensions to specify file types (default: .hpp, .h, .cpp)"
+
+DEFAULT_SPECIAL_COMMENTS_FOR_PVS_STUDIO = (
+    "// This is a personal academic project. Dear PVS-Studio, please check it.\n"
+    "// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com\n\n"
+)
+
+DEFAULT_FILE_EXTENSIONS = (".hpp", ".h", ".cpp")
+# --------------------------------------------------------------- #
